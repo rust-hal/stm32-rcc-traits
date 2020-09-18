3a6821100a067332cecd2086bd3765b3deea6a99
Boris Vinogradov <no111u3@gmail.com>
2020-09-18 10:14:32 +0300

    Add HSE clock representation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+use stm32_utility_traits::time::Hertz;
+
 /// HSE Clock modes
 ///     * `Oscillator`: Use of an external crystal/ceramic resonator
 ///     * `Bypass`: Use of an external user clock
@@ -7,6 +9,26 @@
 pub enum HSEClockMode {
     Oscillator,
     Bypass,
+}
+
+/// HSE Clock
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct HSEClock {
+    pub freq: u32,
+    pub mode: HSEClockMode,
+}
+
+impl HSEClock {
+    /// Provide HSE frequency. Must be between 4 and 26 MHz
+    pub fn new<F>(freq: F, mode: HSEClockMode) -> Self
+    where
+        F: Into<Hertz>,
+    {
+        let f: u32 = freq.into().0;
+
+        assert!(4_000_000 <= f && f <= 26_000_000);
+        HSEClock { freq: f, mode }
+    }
 }
 
 /// Bus associated to peripheral control registers
